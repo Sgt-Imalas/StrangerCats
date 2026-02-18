33aeee2a24f78e4f847cd4d82794ec48f6a09f29
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-02-18 16:12:52 +0100

    Pointery Pointers

--- Assets/Scripts/UI/POI_Pointer.cs
+++ Assets/Scripts/UI/POI_Pointer.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
-public class Pointer : MonoBehaviour
+public class POI_Pointer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	public GameObject Player;
+	public GameObject Target;
+	public Vector3 TargetPosition;
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        var startPos = Player.transform.position;
+        var targetPos = Target?.transform.position ?? TargetPosition; 
+        var direction = targetPos - startPos;
+		float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float distanceFromShip = 300 + Mathf.Clamp(direction.magnitude / 10f, 0, 150);
+        var pos = Quaternion.Euler(0, 0, targetAngle) * (Vector3.right * distanceFromShip);
+
+		transform.localPosition = pos;
+		transform.rotation = Quaternion.Euler(0f, 0f, targetAngle-90);
+	}
 }
