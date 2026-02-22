6879a9de36486baa4b60cdddd2ff349a0c5522d5
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-02-22 18:19:28 +0100

    Fix pawtal not rekindling

--- Assets/Scripts/AttributeTypes.cs
+++ Assets/Scripts/AttributeTypes.cs
@@ -17,7 +17,6 @@
 
 		RadarRange,
 		SpaceShipSuperCruiseSpeed, 
-		SpaceShipSuperCruiseAccelleration,
 		SpaceShipRotationSpeed, 
 	}
 }
--- Assets/Scripts/World/PortalIgniter.cs
+++ Assets/Scripts/World/PortalIgniter.cs
@@ -7,16 +7,19 @@ public class PortalIgniter : MonoBehaviour
 	public GameObject PortalLight;
     void Start()
     {
-		if(!Global.Instance.Upgrades.MeatWorldItemFound || !Global.Instance.Upgrades.TennisWorldItemFound || !Global.Instance.Upgrades.DesertWorldItemFound)
+		if (!Global.Instance.Upgrades.MeatWorldItemFound || !Global.Instance.Upgrades.TennisWorldItemFound || !Global.Instance.Upgrades.DesertWorldItemFound)
 		{
 			Debug.Log("Started listening to item collection for warp portal opening");
 			InvokeRepeating("TryRekindlePortal", 5, 5f);
 		}
+		else
+			TryRekindlePortal();
+
 	}
 
 	private void TryRekindlePortal()
 	{
-		if(Global.Instance.Upgrades.MeatWorldItemFound && Global.Instance.Upgrades.TennisWorldItemFound && Global.Instance.Upgrades.DesertWorldItemFound)
+		if(Global.Instance.Upgrades.MeatWorldItemFound && Global.Instance.Upgrades.TennisWorldItemFound && Global.Instance.Upgrades.DesertWorldItemFound && PortalLight.gameObject.activeSelf)
 		{
 			if (RekindlePortal)
 				MusicManager.PlayFx(RekindlePortal);
