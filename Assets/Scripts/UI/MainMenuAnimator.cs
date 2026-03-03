4f1125f93e506ffe17119839982904e0a612972d
Aki Senkinn <akitary.art@gmail.com>
2026-03-03 21:51:21 +0100

    Main Menu cleanup

--- Assets/Scripts/UI/MainMenuAnimator.cs
+++ Assets/Scripts/UI/MainMenuAnimator.cs
@@ -1,9 +1,7 @@
-using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MainMenuAnimator : MonoBehaviour
 {
@@ -12,11 +10,12 @@ public class MainMenuAnimator : MonoBehaviour
 	public GameObject Initializer;
 	[SerializeField] public List<CatPawButton> buttons;
 	public AnimationCurve scaleCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
-	// Start is called once before the first execution of Update after the MonoBehaviour is created
+
 	void Start()
 	{
 		if (buttons == null)
 			buttons = GetComponentsInChildren<CatPawButton>().ToList();
+
 		foreach (var item in buttons)
 		{
 			item.gameObject.SetActive(false);
@@ -27,32 +26,40 @@ public class MainMenuAnimator : MonoBehaviour
 
 	IEnumerator ButtonSpawnAnimation()
 	{
-		if(Initializer != null)
-		yield return UnRevealButtonAnim(Initializer);
+		if (Initializer != null)
+			yield return UnRevealButtonAnim(Initializer);
+
 		yield return null;
+
 		foreach (var item in buttons)
 		{
 			yield return new WaitForSecondsRealtime(buttonSpawnDelay);
 			yield return RevealButtonAnim(item.gameObject);
 		}
-		buttons.First()?.Select();
+
+		var firstButton = buttons.First();
+
+		if (firstButton != null)
+			firstButton.Select();
 	}
 
 	IEnumerator UnRevealButtonAnim(GameObject toUnReveal)
 	{
 		var startState = toUnReveal.transform.localScale;
 		toUnReveal.transform.localScale = startState;
-		float timer = 0, duration = 0.15f;
+		float timer = 0.0f, duration = 0.15f;
 		var endState = new Vector2(0, 1);
+
 		while (timer <= duration)
 		{
-			float t = Mathf.Clamp01(timer / duration);
-			float curvedT = scaleCurve.Evaluate(t);
+			var t = Mathf.Clamp01(timer / duration);
+			var curvedT = scaleCurve.Evaluate(t);
 
 			toUnReveal.transform.localScale = Vector3.Lerp(startState, endState, curvedT);
 			timer += Time.unscaledDeltaTime;
 			yield return null;
 		}
+
 		toUnReveal.transform.localScale = endState;
 		toUnReveal.SetActive(false);
 	}
@@ -64,15 +71,17 @@ public class MainMenuAnimator : MonoBehaviour
 		button.transform.localScale = startState;
 		float timer = 0, duration = buttonSpawnTime;
 		var endState = Vector3.one;
+
 		while (timer <= duration)
 		{
-			float t = Mathf.Clamp01(timer / duration);
-			float curvedT = scaleCurve.Evaluate(t);
+			var t = Mathf.Clamp01(timer / duration);
+			var curvedT = scaleCurve.Evaluate(t);
 
 			button.transform.localScale = Vector3.Lerp(startState, endState, curvedT);
 			timer += Time.unscaledDeltaTime;
 			yield return null;
 		}
+
 		button.transform.localScale = endState;
 	}
 
